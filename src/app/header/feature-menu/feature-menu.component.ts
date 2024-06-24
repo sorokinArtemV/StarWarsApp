a5974bff3b575d280c3495406cbae239722cff2b
artem <artem.sorkin.v@gmail.com>
2024-06-25 04:36:04 +0500

    Add feature-button component

--- src/app/header/feature-menu/feature-menu.component.ts
+++ src/app/header/feature-menu/feature-menu.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { FeatureButtonComponent } from '../../shared/feature-button/feature-button.component';
 
 @Component({
   selector: 'app-feature-menu',
   standalone: true,
-  imports: [],
+  imports: [
+    FeatureButtonComponent
+  ],
   templateUrl: './feature-menu.component.html',
   styleUrl: './feature-menu.component.scss'
 })
--- src/app/shared/feature-button/feature-button.component.ts
+++ src/app/shared/feature-button/feature-button.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-feature-button',
+  selector: 'button[appFeatureButton]',
   standalone: true,
   imports: [],
   templateUrl: './feature-button.component.html',
-  styleUrl: './feature-button.component.scss'
+  styleUrl: './feature-button.component.scss',
+  host: {
+    'class': 'feature-menu__button feature-menu-button button button--transparent'
+  }
 })
 export class FeatureButtonComponent {
 
